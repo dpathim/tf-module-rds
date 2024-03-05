dd9d35b234bd1f57fb4b73b50c984af7e39bb028
Deepthi <dpdevops6@gmail.com>
2024-03-05 15:46:17 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -46,11 +46,11 @@ resource "aws_rds_cluster" "main" {
   tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
 }
 
-#resource "aws_rds_cluster_instance" "cluster_instances" {
-#  count              = 2
-#  identifier         = "aurora-cluster-demo-${count.index}"
-#  cluster_identifier = aws_rds_cluster.default.id
-#  instance_class     = "db.r4.large"
-#  engine             = aws_rds_cluster.default.engine
-#  engine_version     = aws_rds_cluster.default.engine_version
-#}
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = var.instance_count
+  identifier         = "${local.name_prefix}-cluster-instance-${count.index+1}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = var.instance_class
+  engine             = aws_rds_cluster.main.engine
+  engine_version     = aws_rds_cluster.main.engine_version
+}
